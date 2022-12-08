8f03f1f3390e3d6198dd73186fb7b520c24ed90e
Raj R <58966550+rajrku@users.noreply.github.com>
2022-12-08 13:45:40 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ set -x
 
 cat $(pwd)/env/.bashrc >> $HOME/.bashrc
 ln -s $(pwd)/env/startup.sh $HOME/startup.sh
+ln -s $(pwd)/env/status.sh $HOME/status.sh
 mkdir -p $HOME/.config/powershell
 cp $(pwd)/env/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
