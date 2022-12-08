1406846204921f42338ab85b62d7c32a8444885e
Raj R <58966550+rajrku@users.noreply.github.com>
2022-12-08 13:32:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,7 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/vimrc $HOME/.vimrc
-ln -s $(pwd)/vim $HOME/.vim
-ln -s $(pwd)/emacs $HOME/.emacs
-ln -s $(pwd)/screenrc $HOME/.screenrc
-
-vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+cat $(pwd)/env/.bashrc >> $HOME/.bashrc
+ln -s $(pwd)/env/startup.sh $HOME/startup.sh
+mkdir -p $HOME/.config/powershell
+cp $(pwd)/env/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
