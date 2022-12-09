b9fa05c6eca9418ca156ff3add787fd00ef970f3
Raj R <58966550+rajrku@users.noreply.github.com>
2022-12-09 12:01:31 -0800

    Update deployRunnerBinaries.sh

--- env/deployRunnerBinaries.sh
+++ env/deployRunnerBinaries.sh
@@ -1,5 +1,5 @@
 b
-if  [[$? -ne 0]]
-then
+if  ($? -ne 0)
+{
   deploy -DeployAzureRelay runner -UseExistingDatabases; kubectl get all | grep -i pod/runner-job | cut -d " " -f1 | xargs kubectl logs -f
-fi
+}
