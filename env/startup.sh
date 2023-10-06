5ff654bd5584ad3f68dc8d62192cfe1664e9f294
Raj R <58966550+rajrku@users.noreply.github.com>
2023-10-06 13:24:40 -0700

    Update startup.sh

--- env/startup.sh
+++ env/startup.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
-echo "Setting up workspace..."
-echo "Starting github service..."
 
+start-actions
 GH_STATUS=`service github status`
 echo "Github service status: " $GH_STATUS
 
 if [[ "$GH_STATUS" == "Stopped" ]]
 then
-  # nohup service github start 
-  cd /workspaces/github
-  nohup script/server &
-  service github start
-  service launch start
+
+	( cd $SKYRISE_PATH && script/build && script/setup-codespaces-runner.ps1 && reset-tenant-mapping )
+
+	# Ensure Launch/Kreds are started in the background
+	( service launch start && service kredz-service start )
+
+	# Start dotcom in the background
+	(cd $GITHUB_PATH && script/dx/server-start )
 fi
 
-echo "Successfully started github service..."
-echo "Starting actions services..."
-minikube profile actions-dev
-minikube status
+# echo "Setting up workspace..."
+# echo "Starting github service..."
 
-kubectl get pods | grep -i runner 
+# GH_STATUS=`service github status`
+# echo "Github service status: " $GH_STATUS
 
-if [[ $? -ne 0 ]]
-then
-  echo "Starting actions service..."
-  # nohup /workspaces/github/script/actions/start-actions
-  cd /workspaces/actions/actions-dotnet/src/
-  script/setup-codespaces-runner.ps1
-fi
-echo "Successfully started actions services..."
+# if [[ "$GH_STATUS" == "Stopped" ]]
+# then
+#   # nohup service github start 
+#   cd /workspaces/github
+#   nohup script/server &
+#   service github start
+#   service launch start
+# fi
+
+# echo "Successfully started github service..."
+# echo "Starting actions services..."
+# minikube profile actions-dev
+# minikube status
+
+# kubectl get pods | grep -i runner 
+
+# if [[ $? -ne 0 ]]
+# then
+#   echo "Starting actions service..."
+#   # nohup /workspaces/github/script/actions/start-actions
+#   cd /workspaces/actions/actions-dotnet/src/
+#   script/setup-codespaces-runner.ps1
+# fi
+# echo "Successfully started actions services..."
