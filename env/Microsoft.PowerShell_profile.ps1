2020d1cd4ba0dc8e117d2107cdcb542bc9a94fbd
Raj R <58966550+rajrku@users.noreply.github.com>
2024-06-03 10:37:53 -0700

    Update Microsoft.PowerShell_profile.ps1

--- env/Microsoft.PowerShell_profile.ps1
+++ env/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 #This file should be located in ~/.config/powershell/Microsoft.PowerShell_profile.ps1
 #Setting AzureRegion helps prevent us from bumping up against our per-region storage quotas.
-$env:AzureRegion = "westus2"
-$env:AzureSubscriptionId = "6d3acc75-039d-4a84-a0c2-883e492a72dd"   #This line isn't needed if you're only testing OnPrem.
+# $env:AzureRegion = "westus2"
+# $env:AzureSubscriptionId = "6d3acc75-039d-4a84-a0c2-883e492a72dd"   #This line isn't needed if you're only testing OnPrem.
 
 function co { git checkout users/rajenr/$args }
 function coma { git checkout master; git pull }
